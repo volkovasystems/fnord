cda9cbd2f401ab7eab755c02c05db3a471020ced
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-08 11:52:26 +0800

    Updated test.

--- fnord-test.js
+++ fnord-test.js
@@ -2,7 +2,13 @@
 const assert = require( "assert" );
 const fnord = require( "./fnord.js" );
 
+assert.deepEqual( fnord( ), [ ], "should return empty array" );
+
+assert.deepEqual( fnord( [ ] ), [ ], "should return empty array" );
+
 assert.ok( fnord( [ 1, 2, 3 ] ) );
+
+assert.ok( fnord( [ 1, 2, 3, [ 4, 5, 6 ] ] ) );
 
 assert.equal( Array.isArray( fnord( [ 1, 2, 3 ] ) ), true, "should be true" );
 
